d1095844b20b97d4c5075f48416549d3d19d6d64
sübhan akbenli <suakbenli@gmail.com>
2023-11-11 03:12:12 +0300

    ++++

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ hayvanEkle_main_window = QMainWindow()
 hayvanEkle_ui =  Ui_hayvanEkle_MainWindow()
 hayvanEkle_ui.setupUi(hayvanEkle_main_window)
 
+from hayvanBilgisi_ui import *
+hayvanBilgisi_main_window = QMainWindow()
+hayvanBilgisi_ui =  Ui_hayvanBilgisi_MainWindow()
+hayvanBilgisi_ui.setupUi(hayvanBilgisi_main_window)
+
 conn=sqlite3.connect("database.db")
 curs=conn.cursor()
 curs.execute("CREATE TABLE IF NOT EXISTS hayvanlar(kimlikNo INTEGER PRIMARY KEY AUTOINCREMENT,cins Text,cinsiyet Text,dogumTarihi Date,agirlik Float,saglikDurumu Text,ekstraBilgiler Text,konum Text)")
-
 
 
 # QR kod oluşturacak fonksiyon
@@ -43,6 +47,7 @@ def Qr_olustur(data):
 
     # Görüntüyü kaydet
     img.save(f"QR/{data}.png")  # veya "barkod.jpg" olarak kaydedebilirsiniz
+
 # Qr okuma işlemi için bir fonksiyon
 def Qr_oku(image_path):
     import cv2
@@ -104,7 +109,7 @@ def Qr_oku_video(konum):
     # Kamera kapatma
     cap.release()
     cv2.destroyAllWindows()
-    
+    hayvanListele()
 
 def hayvanEkle():
     cins=hayvanEkle_ui.cins_lineEdit.text()
@@ -127,7 +132,13 @@ def hayvanListele(icerde=True,disarda=True,erkek=True,disi=True):
     data = curs.fetchall()
     tabloDoldur(data,icerde,disarda,erkek,disi)
 
-def tabloDoldur(data,icerde,disarda,erkek,disi):
+def kimlikleAra(kimlikNo):
+    curs.execute("SELECT kimlikNo,cins,cinsiyet,dogumTarihi,agirlik,saglikDurumu,ekstraBilgiler,konum FROM hayvanlar Where kimlikNo = ?",(kimlikNo,))
+    data = curs.fetchall()
+    tabloDoldur(data)
+       
+ 
+def tabloDoldur(data,icerde=True,disarda=True,erkek=True,disi=True):
     anaSayfa_ui.tableWidget.clearContents()
     list1=[]
     list2=[]
@@ -142,6 +153,7 @@ def tabloDoldur(data,icerde,disarda,erkek,disi):
     counter=0
     for kimlikNo,cins,cinsiyet,dogumTarihi,agirlik,saglikDurumu,ekstraBilgiler,konum in data:
         if (konum in list1) and (cinsiyet in list2):
+                
             counter+=1
             anaSayfa_ui.tableWidget.setRowCount(counter)
             anaSayfa_ui.tableWidget.setItem(counter-1,0,QTableWidgetItem(str(kimlikNo)))
@@ -151,17 +163,46 @@ def tabloDoldur(data,icerde,disarda,erkek,disi):
             anaSayfa_ui.tableWidget.setItem(counter-1,4,QTableWidgetItem(str(agirlik)))
             anaSayfa_ui.tableWidget.setItem(counter-1,5,QTableWidgetItem(str(saglikDurumu)))
             anaSayfa_ui.tableWidget.setItem(counter-1,6,QTableWidgetItem(str(konum)))
-    anaSayfa_ui.tableWidget
+            
+            if konum=="İçeride":
+                tablo_row_renklendir(anaSayfa_ui.tableWidget,counter-1,QtGui.QColor(25, 240, 60))
+            if konum=="Dışarıda":
+                tablo_row_renklendir(anaSayfa_ui.tableWidget,counter-1,QtGui.QColor(240, 20, 60))              
+
+def tablo_row_renklendir(tablo,row,color):
+    for col in range(tablo.columnCount()+1):
+        item = tablo.item(row, col)
+        if item:
+            item.setBackground(color)    
         
 
 def hayvanEkle_Ac():
     hayvanEkle_main_window.show()
 
+def bilgiGuncelle():
+    kimlikNo=hayvanBilgisi_main_window.windowTitle()
+    yeni_ekstra = hayvanBilgisi_ui.ekstraBilgiler_textEdit.toPlainText() 
+    curs.execute("UPDATE hayvanlar SET ekstraBilgiler = ? WHERE kimlikNo = ?", (yeni_ekstra,kimlikNo))
+    conn.commit()
+    
+def bilgiGetir():
+    hayvanBilgisi_main_window.show()
+    sender_combo = anaSayfa_main_window.sender()
+    if sender_combo:
+        try:
+            kimlikNo = anaSayfa_ui.tableWidget.selectedItems()[0].text()
+        except:
+            kimlikNo = -1
+        hayvanBilgisi_main_window.setWindowTitle(str(kimlikNo))
+        curs.execute("SELECT ekstraBilgiler FROM hayvanlar WHERE kimlikNo = ?", (kimlikNo,))
+        data=curs.fetchone()
+        for i in data:
+                hayvanBilgisi_ui.ekstraBilgiler_textEdit.setText(i)
+                
+hayvanBilgisi_ui.ekstraBilgiler_textEdit.textChanged.connect(lambda : bilgiGuncelle())
 
-
-
-
-
+anaSayfa_ui.tableWidget.doubleClicked.connect(lambda : bilgiGetir())
+anaSayfa_ui.kimlikNoAra_pushButton.clicked.connect(lambda : kimlikleAra(anaSayfa_ui.kimlikNo_lineEdit.text()))
 anaSayfa_ui.hayvanEkle_pushButton.clicked.connect(lambda : hayvanEkle_Ac())
 anaSayfa_ui.QrOkut_giris.clicked.connect(lambda :   Qr_oku_video("İçeride") )
 anaSayfa_ui.QrOkut_cikis.clicked.connect(lambda :   Qr_oku_video("Dışarıda") )
